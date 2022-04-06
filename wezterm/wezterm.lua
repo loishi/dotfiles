db13f246b58f0a74693ea6250f9ed58a4be42e98
loishi <loishi1312@gmail.com>
2022-04-06 08:38:46 -0700

    Modified wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm';
 
 return {
   font = wezterm.font("Hack Nerd Font"),
-  window_background_opacity = 0.9,
-  text_background_opacity = 0.3,
+  window_background_opacity = 0.75,
+  text_background_opacity = 0.2,
   color_scheme = "Tomorrow Night",
   font_size = 16.0,
 }
