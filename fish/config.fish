da26a7ff9fba31dfb4be95ddac92e33a0f7b3c3a
loishi <loishi1312@gmail.com>
2022-04-08 13:21:38 -0700

    Modified config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,9 @@ set MANPATH 	/usr/local/texlive/2022/texmf-dist/doc/man $MANPATH
 set INFOPATH 	/usr/local/texlive/2022/texmf-dist/doc/info $INFOPATH
 set -g -x GPG_TTY (tty)
 set -g -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-
-# TeX
-starship init fish | source
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -x fish_user_paths /Library/TeX/texbin $fish_user_paths
+
 alias ssh "env TERM=xterm ssh"
 alias ll "exa -al"
 alias nv "nvim"
@@ -15,8 +14,9 @@ alias bu "sudo tlmgr update -all -self --reinstall-forcibly-removed && brew upda
 alias gpp "g++ main.cc && ./a.out"   
 alias aud "sudo launchctl stop com.apple.audio.coreaudiod && sudo launchctl start com.apple.audio.coreaudiod"  
 alias acs "acc s -s -- -y"
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
 gpgconf --launch gpg-agent
+starship init fish | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/loishi/google-cloud-sdk/path.fish.inc' ]; . '/Users/loishi/google-cloud-sdk/path.fish.inc'; end
