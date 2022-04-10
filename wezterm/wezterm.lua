771a24fb090fbfba902816ecc51d993568c4dad8
loishi <loishi1312@gmail.com>
2022-04-10 01:38:04 -0700

    modified wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font_with_fallback({"Hack", "HackGenNerd Console", "Source Han Mono" }),
+  font = wezterm.font_with_fallback({"Hack", "HackGenNerd Console"}),
   window_background_opacity = 0.8,
   window_decorations = "RESIZE",
   enable_tab_bar = false,
-  text_background_opacity = 0.2,
+  text_background_opacity = 0.1,
   color_scheme = "Tomorrow Night",
   font_size = 16.0,
   window_close_confirmation = "NeverPrompt",
